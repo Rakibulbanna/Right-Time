7ef9ca2a26c2cfe31253c9447cbf2b1e3d59698e
Rakib ul banna <rakib35-2638@diu.edu.bd>
2022-10-28 03:37:53 +0600

    partner all table insert API done

--- src/routeHandler/PartnerHandle.js
+++ src/routeHandler/PartnerHandle.js
@@ -44,41 +44,83 @@ const firstInsert = async (e) => {
 };
 
 router.post("/addServicePartner", async (req, res) => {
-  const data = await Partners.countDocuments({});
-  if (data >= 1) {
-    const id = await Partners.find({}).select("_id");
+  const dataCount = await Partners.countDocuments({});
 
-    await Partners.updateOne(
-      { _id: id[0]._id.toString() },
-      {
-        $push: {
-          ServicePartner: {
-            name: req.body?.ServicePartnerName || "",
-            url: req.body?.ServicePartnerUrl || "",
+  if (dataCount === 1) {
+    const dd = await Partners.find({}).select({
+      Association: 0,
+      SolutionPartner: 0,
+      reply: 0,
+      __v: 0,
+    });
+const count = await dd[0].ServicePartner.length;
+
+const flagName =  await dd[0].ServicePartner[0].name;
+const flagUrl = await dd[0].ServicePartner[0].url;
+
+    if (count === 1 && flagName ==="" && flagUrl==="") {
+//console.log(dd[0].SolutionPartner[0]._id.toString())
+const p_id = await dd[0]._id.toString();
+const c_id = await dd[0].ServicePartner[0]._id.toString();
+
+console.log(p_id,"   ",c_id)
+
+      await Partners.findOneAndUpdate({
+        _id: p_id, "ServicePartner._id": c_id
+      },
+        {
+          $set: {
+            "ServicePartner.$.name": req.body?.ServicePartnerName,
+            "ServicePartner.$.url": req.body?.ServicePartnerUrl,
           },
         },
-      },
-      (err, data) => {
-        if (err) {
-          res.status(500).send("ServicePartner insertion failed!!");
-        } else {
-          console.log(data);
-          res
-            .status(200)
-            .send({ data: data, message: "ServicePartner inserted!!" });
+        {
+          useFindAndModify: false,
+          new: true,
         }
-      }
-    );
-  } else {
-    await firstInsert(req.body);
-    const id = await Partners.find({}).select("_id ServicePartner");
+      ).exec((err, data) => {
+        if (err) {
+          res.status(500).json({ message: "ServicePartner updated failed!!" });
+        } else {
+          res.status(200).json({
+            message: "ServicePartner updated successfully",
+            data: data,
+          });
+        }
+      });
+    }
+     else {
+      
+      const id = dd[0]._id.toString() 
 
+      await Partners.updateOne(
+        { _id: id},
+        {
+          $push: {
+            ServicePartner: {
+              name: req.body?.ServicePartnerName || "",
+              url: req.body?.ServicePartnerUrl || "",
+            },
+          },
+        },
+        (err, data) => {
+          if (err) {
+            res.status(500).send("ServicePartner insertion failed!!");
+          } else {
+            // console.log(data)
+            res.status(200).send({ data: data, message: "ServicePartner inserted!!" });
+          }
+        }
+      );
+    }
+  } 
+else {
+    await firstInsert(req.body);
     await Partners.findOneAndUpdate(
-      { _id: id },
       {
         $set: {
-          "ServicePartner.$.name": req.body?.ServicePartnerName,
-          "ServicePartner.$.url": req.body?.ServicePartnerUrl,
+          "ServicePartnerName.$.name": req.body?.ServicePartnerName,
+          "ServicePartnerUrl.$.url": req.body?.ServicePartnerUrl,
         },
       },
       {
@@ -108,21 +150,24 @@ router.post("/addSolutionPartner", async (req, res) => {
       reply: 0,
       __v: 0,
     });
+const count = await dd[0].SolutionPartner.length;
+const flagName =  await dd[0].SolutionPartner[0].name;
+const flagUrl = await dd[0].SolutionPartner[0].url;
 
-    const count = await dd[0].SolutionPartner.length;
-
-    if (count === 1) {
+    if (count === 1 && flagName ==="" && flagUrl==="") {
 //console.log(dd[0].SolutionPartner[0]._id.toString())
-const p_id = dd[0]._id.toString();
-const c_id =dd[0].SolutionPartner[0]._id.toString();
+const p_id = await dd[0]._id.toString();
+const c_id = await dd[0].SolutionPartner[0]._id.toString();
+
 console.log(p_id,"   ",c_id)
+
       await Partners.findOneAndUpdate({
         _id: p_id, "SolutionPartner._id": c_id
       },
         {
           $set: {
-            "SolutionPartnerName.$.name": req.body?.SolutionPartnerName,
-            "SolutionPartnerUrl.$.url": req.body?.SolutionPartnerUrl,
+            "SolutionPartner.$.name": req.body?.SolutionPartnerName,
+            "SolutionPartner.$.url": req.body?.SolutionPartnerUrl,
           },
         },
         {
@@ -141,9 +186,10 @@ console.log(p_id,"   ",c_id)
       });
     }
      else {
-      const id = await Partners.find({}).select("_id");
+      const id = dd[0]._id.toString() 
+
       await Partners.updateOne(
-        { _id: id[0]._id.toString() },
+        { _id: id},
         {
           $push: {
             SolutionPartner: {
@@ -157,14 +203,13 @@ console.log(p_id,"   ",c_id)
             res.status(500).send("SolutionPartner insertion failed!!");
           } else {
             // console.log(data)
-            res
-              .status(200)
-              .send({ data: data, message: "SolutionPartner inserted!!" });
+            res.status(200).send({ data: data, message: "SolutionPartner inserted!!" });
           }
         }
       );
     }
-  } else {
+  } 
+else {
     await firstInsert(req.body);
     await Partners.findOneAndUpdate(
       {
@@ -183,6 +228,101 @@ console.log(p_id,"   ",c_id)
       } else {
         res.status(200).json({
           message: "SolutionPartner inserted successfully",
+          data: data,
+        });
+      }
+    });
+  }
+});
+
+router.post("/addAssociation", async (req, res) => {
+  const dataCount = await Partners.countDocuments({});
+
+  if (dataCount === 1) {
+    const dd = await Partners.find({}).select({
+      SolutionPartner: 0,
+      ServicePartner: 0,
+      reply: 0,
+      __v: 0,
+    });
+const count = await dd[0].Association.length;
+const flagName =  await dd[0].Association[0].name;
+const flagUrl = await dd[0].Association[0].url;
+
+    if (count === 1 && flagName ==="" && flagUrl==="") {
+//console.log(dd[0].SolutionPartner[0]._id.toString())
+const p_id = await dd[0]._id.toString();
+const c_id = await dd[0].Association[0]._id.toString();
+
+console.log(p_id,"   ",c_id)
+
+      await Partners.findOneAndUpdate({
+        _id: p_id, "Association._id": c_id
+      },
+        {
+          $set: {
+            "Association.$.name": req.body?.AssociationName,
+            "Association.$.url": req.body?.AssociationUrl,
+          },
+        },
+        {
+          useFindAndModify: false,
+          new: true,
+        }
+      ).exec((err, data) => {
+        if (err) {
+          res.status(500).json({ message: "Association updated failed!!" });
+        } else {
+          res.status(200).json({
+            message: "Association updated successfully",
+            data: data,
+          });
+        }
+      });
+    }
+     else {
+      const id = dd[0]._id.toString() 
+
+      await Partners.updateOne(
+        { _id: id},
+        {
+          $push: {
+            Association: {
+              name: req.body?.AssociationName || "",
+              url: req.body?.AssociationUrl || "",
+            },
+          },
+        },
+        (err, data) => {
+          if (err) {
+            res.status(500).send("Association insertion failed!!");
+          } else {
+            // console.log(data)
+            res.status(200).send({ data: data, message: "Association inserted!!" });
+          }
+        }
+      );
+    }
+  } 
+else {
+    await firstInsert(req.body);
+    await Partners.findOneAndUpdate(
+      {
+        $set: {
+          "AssociationName.$.name": req.body?.AssociationName,
+          "AssociationUrl.$.url": req.body?.AssociationUrl,
+        },
+      },
+      {
+        useFindAndModify: false,
+        new: true,
+      }
+    ).exec((err, data) => {
+      if (err) {
+        res.status(500).json({ message: "Association updated failed!!" });
+      } else {
+        res.status(200).json({
+          message: "Association inserted successfully",
           data: data,
         });
       }
