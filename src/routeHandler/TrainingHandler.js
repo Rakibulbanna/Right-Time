c4ab06ef8affda11fb85c32f475e6befc899557d
Rakib ul banna <rakib35-2638@diu.edu.bd>
2022-11-01 02:27:34 +0600

    delete trainning API

--- src/routeHandler/TrainingHandler.js
+++ src/routeHandler/TrainingHandler.js
@@ -55,7 +55,7 @@ router.get("/allSecurity", async (req, res) => {
   }
 });
 
-//DONE post
+//Edit Needed post
 
 router.post("/addAssessment", async (req, res) => {
   const NewAssessment = new Assessment(req.body);
@@ -226,4 +226,66 @@ router.post("/addSecurity", async (req, res) => {
   }
 });
 
+// DONE delete
+
+router.delete("/Assessment/:id",async(req,res)=>{
+  try{
+    await Assessment.deleteOne({ _id:  req.params.id})
+    res.status(200).send("Assessment deleted!")
+  }
+  catch(err){
+  if (err) {
+      res.status(500).json({ message: "Assessment deletion failed!!" });
+    } else {
+      //console.log(data);
+      res.status(200).json({ message: "Assessment deleted successfully!!" });
+    }
+  
+  }
+});
+router.delete("/Customized/:id",async(req,res)=>{
+  try{
+    await Customized.deleteOne({ _id:  req.params.id})
+    res.status(200).send("Customized deleted!")
+  }
+  catch(err){
+  if (err) {
+      res.status(500).json({ message: "Customized deletion failed!!" });
+    } else {
+      //console.log(data);
+      res.status(200).json({ message: "Customized deleted successfully!!" });
+    }
+  
+  }
+});
+router.delete("/Management/:id",async(req,res)=>{
+  try{
+    await Management.deleteOne({ _id:  req.params.id})
+    res.status(200).send("Management deleted!")
+  }
+  catch(err){
+  if (err) {
+      res.status(500).json({ message: "Management deletion failed!!" });
+    } else {
+      //console.log(data);
+      res.status(200).json({ message: "Management deleted successfully!!" });
+    }
+  
+  }
+});
+router.delete("/Security/:id",async(req,res)=>{
+  try{
+    await Security.deleteOne({ _id:  req.params.id})
+    res.status(200).send("Security deleted!")
+  }
+  catch(err){
+  if (err) {
+      res.status(500).json({ message: "Security deletion failed!!" });
+    } else {
+      //console.log(data);
+      res.status(200).json({ message: "Security deleted successfully!!" });
+    }
+  
+  }
+});
 module.exports = router;
